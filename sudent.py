fc1206695187513b04946180c3b793fa398a9ba9
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2026-02-25 10:44:00 +0100

    Update sudent.py

--- sudent.py
+++ sudent.py
@@ -1,9 +1,11 @@
 class Student:
-    name = ''
-    age = 0
-    height = 0
-    gender = ''
-    score = 0
+
+    def __init__(self, name, age, height, gender, score):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.gender = gender
+        self.score = score
 
     def learn(self, test):
         self.score += test
@@ -11,14 +13,14 @@ class Student:
     def __str__(self):
         return(f'Hello! I am {self.name}, {self.age} age, {self.height} tall, sex : {self.gender}, {self.score} scorecard')
 
-Lajos = Student()
-Lajos.name = 'Lajos'
-Lajos.age = 13
-Lajos.height = 178
-Lajos.gender = 'Megatron'
-Lajos.score = 1
-
-Anna = Student()
+Lajos = Student('Lajos', 13, 178, 'Megatron', 0)
 
 print(Lajos)
+
+Anna = Student('Anna', 15, 170, 'Female', 100)
+
 print(Anna)
+
+Lajos.learn(100)
+
+print(Lajos.score)
