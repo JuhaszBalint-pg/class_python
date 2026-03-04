b238c8ceb1d633ca9597984a4b52b56a861217e3
JuhaszBalint-pg <juhasz.balint@diak.szbi-pg.hu>
2026-03-04 09:36:11 +0100

    autok listában kezelése
    random sebesség hozzáadása, átlageletkor, legidősebb auto

--- 2026_03_04/maintwo.py
+++ 2026_03_04/maintwo.py
@@ -1,11 +1,40 @@
+import random
 from auto import AUTO
 
-COROLLA = AUTO('Toyota', 'Corolla', 2015, sebesseg_= 0)
+auto1 = AUTO('Toyota', 'Corolla', 2015, sebesseg_= 0)
+auto2 = AUTO('Porsche', '918', 2018, sebesseg_=0)
+auto3 = AUTO('Audi', 'E-Tron GT', 2020, sebesseg_=0)
+auto4 = AUTO('Ford', 'Focus', 1999, sebesseg_=0)
+auto5 = AUTO('Volkswagen', 'Scirocco', 2008, sebesseg_=0)
 
-Porsche = AUTO('Porsche', '918', 2018, sebesseg_=0)
+autok = [auto1, auto2, auto3, auto4, auto5]
+for auto in autok:
+    print(auto)
 
-COROLLA.gyorsit(30)
-COROLLA.fekez(10)
+for auto in autok:
+    auto.gyorsit(random.randint(30, 150))
 
-print(COROLLA)
-print(Porsche)
+for auto in autok:
+    print(auto.sebesseg)
+
+autok_szama = len(autok)
+osszeletkor = 0
+for auto in autok:
+    osszeletkor += 2026 - auto.gyartasiev
+
+atlageletkor = osszeletkor/autok_szama
+print(atlageletkor)
+
+autokeletkora = []
+for auto in autok:
+    kor = 2026-auto.gyartasiev
+    autokeletkora.append(kor)
+
+legidosebb_auto = autokeletkora.index(max(autokeletkora))
+print(f'A legidősebb autó eletkora {max(autokeletkora)} {autok[legidosebb_auto]}')
+
+ #c version
+gyartasi_evek = [auto.gyartasiev for auto in autok]
+for auto in autok:
+    if auto.gyartasiev == min(gyartasi_evek):
+        print(f'A legidosebb auto = {auto}')
